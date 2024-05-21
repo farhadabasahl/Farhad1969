c8a347d7f718e7313fd76df9af4c3a35dbeb737e
Farhad Abasahl <124461281+Farhad1969@users.noreply.github.com>
2024-05-20 23:55:59 -0400

    Added init.sql for database setup and updated .gitignore

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS crypto_prices (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    last_trade NUMERIC,
+    volume NUMERIC,
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
