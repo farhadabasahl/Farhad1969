7ead8628729d6b02e71b36f297d50c6cc7e064f9
Farhad Abasahl <124461281+Farhad1969@users.noreply.github.com>
2024-05-27 00:43:54 -0400

    Remove 'setting' directory, .gitignore, init.sql from the main branch

--- init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS crypto_prices (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50),
-    last_trade NUMERIC,
-    volume NUMERIC,
-    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
